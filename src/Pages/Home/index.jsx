299e1f5a5c6aa61627207fd312396b9b8a21be4f
emarino-git <marinomatias@outlook.com.ar>
2023-10-26 10:42:00 -0300

    link with Fake Store Api

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,25 @@
+import { useState, useEffect } from "react"
 import Card from "../../Components/Card"
 import Layout from "../../Components/Layout"
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res=>res.json())
+      .then(json=>setItems(json))
+  }, [])
 
   return (
     <>
       <Layout className='bg-green-200'>
         Home
-        <Card />
+        <div className='grid gap-3 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          items?.map(item => (<Card key={item.id} data={item} />))
+        }
+        </div>
       </Layout>
     </>
   )
