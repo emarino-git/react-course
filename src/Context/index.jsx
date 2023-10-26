fa82b59f212820647754305d0178937791317861
emarino-git <marinomatias@outlook.com.ar>
2023-10-26 18:26:14 -0300

    Product detail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,13 +3,26 @@ import { createContext, useState } from "react";
 export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({children}) => {
+  // Shopping Cart - Incremetn Quantity
   const [count, setCount] = useState(0)
-  console.log(count)
+
+  // Product Detail - Open/Close
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+  const openProductDetail = () => setIsProductDetailOpen(true)
+  const closeProductDetail = () => setIsProductDetailOpen(false)
+
+  // Product Detail - Show Product
+  const [productToShow, setProductToShow] = useState({})
 
   return (
     <ShoppingCartContext.Provider value={{
       count,
-      setCount
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isProductDetailOpen,
+      productToShow,
+      setProductToShow
     }}>
       {children}
     </ShoppingCartContext.Provider>
